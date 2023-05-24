afcefb109c6b9110cdb81c7a3b554531ec6bd28d
Saveliy <liedwavy@gmail.com>
2023-05-24 20:15:56 +0300

    Add FileCabinetRecord.cs file.

---
+++ FileCabinetApp/FileCabinetRecord.cs
@@ -0,0 +1,10 @@
+﻿public class FileCabinetRecord
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+}
